a29b866119edcafad10815bb8ffc1c68738c6935
zchshen[6~@163.com <https://github.com/zchshen/anoGitHub.git>
2017-03-12 08:57:05 -0700

    modifycalculate_birthday.sh

--- shell_exercise/calculate_birthday.sh
+++ shell_exercise/calculate_birthday.sh
@@ -11,7 +11,7 @@ if [ $bir = $now ];then
 	echo "Happy birthday to you!"
 elif [ $bir -gt $now ];then
 	year=`date +%Y`
-	echo "$Year"
+	#echo "$year"
 	total_d=$(($((`date --date="$year$bir" +%s` - `date +%s`))/60/60/24))
 	echo "Your birthday will be $total_d later"
 else
